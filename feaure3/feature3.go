6cc9441b6ba5509bb2fafad81ccc33522778bc90
True Adam <ceminsinsilovic@gmail.com>
2026-08-14 07:43:46 +0700

    sgdsg3

---
+++ feaure3/feature3.go
@@ -0,0 +1,9 @@
+package feaure3
+
+import "fmt"
+
+func Feature3() {
+	for i := 0; i < 5; i++ {
+		fmt.Println("FEATURE 3")
+	}
+}
--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"study/FirstGit"
 	"study/feature2"
+	"study/feaure3"
 )
 func main(){
 	fmt.Println("Hello Git")
 	firstgit.Tig()
 	feature2.Feature2()
-
+	feaure3.Feature3()
 }
 
